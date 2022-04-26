1ec98266aa9dcc140698f3c48457c330b253f3a6
Jason <leejason1990@gmail.com>
2022-04-25 22:26:19 -0400

    403 error, looking for alternatives

--- api_request.py
+++ api_request.py
@@ -4,12 +4,12 @@ import time
 
 
 file = 0
-token = 'Bearer awezqu488bbrfihoev5yxu4oe2h0yt fields *;'
+token = 'Bearer%20awezqu488bbrfihoev5yxu4oe2h0yt'
 client_id = 'tvm3g7znmadal06thlqf63p41ll5kz'
 
 while file < 50:
     time.sleep(10)
-    url = f'https://api.igdb.com/v4/games?Client-ID={client_id}&Authorization={token}'
+    url = f'https://api.igdb.com/v4/games?Client-ID={client_id}&Authorization={token}%20fields%20*;'
     req = urllib.request.Request(url)
     file_name = '.games'+str(file)+'.json'
 
