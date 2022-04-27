f27795a07dedaaef4d509f102199e462945bedea
Jason <leejason1990@gmail.com>
2022-04-26 22:19:59 -0400

    correcting mistakes

--- api_request.py
+++ api_request.py
@@ -1,20 +1,22 @@
 import json
 import urllib.request
 import time
-
+import os
 
 file = 0
-token = ''
-client_id = ''
+igdb_token = os.environ.get('IGDB_TOKEN')
+igdb_id = os.environ.get('IGDB_ID')
 
-while file < 50:
-    time.sleep(10)
-    url = f'https://api.igdb.com/v4/games?Client-ID={client_id}&Authorization={token}%20fields%20*;'
-    req = urllib.request.Request(url)
-    file_name = '.games'+str(file)+'.json'
 
-    with urllib.request.urlopen(req) as f:
-        data = json.load(f)
 
-        with open(file_name, 'w') as fo:
-            json.dump(data, fo)
+# while file < 50:
+#     time.sleep(10)
+#     url = f'https://api.igdb.com/v4/games?Client-ID={client_id}&Authorization={token}%20fields%20*;'
+#     req = urllib.request.Request(url)
+#     file_name = '.games'+str(file)+'.json'
+#
+#     with urllib.request.urlopen(req) as f:
+#         data = json.load(f)
+#
+#         with open(file_name, 'w') as fo:
+#             json.dump(data, fo)
