52e3027cee6b1a5a75a9c11945a764a5fd1540c6
Jason <leejason1990@gmail.com>
2022-04-27 01:23:15 -0400

    only name and id working

--- api_request.py
+++ api_request.py
@@ -2,7 +2,6 @@ import csv
 import json
 import sys
 import time
-import urllib.request
 import os
 from igdb.wrapper import IGDBWrapper
 
@@ -11,24 +10,19 @@ igdb_token = os.environ.get('IGDB_TOKEN')
 igdb_id = os.environ.get('IGDB_ID')
 
 wrapper = IGDBWrapper(igdb_id, igdb_token)
-byte_array = wrapper.api_request(
-    'games',
-    'fields id, name, genres, rating; offset 0; where platforms=48;',
-)
-
 LIMIT = 4
 
 
 def get_games(last, wrap):
     options = 'fields id, name, age_ratings, aggregated_rating, aggregated_rating_count, cover, first_release_date, ' \
-              'genres, multiplayer_modes, rating, rating_count, storyline, total_rating, total_rating_count;' \
+              'genres, multiplayer_modes, platforms, rating, rating_count, storyline, total_rating, total_rating_count;' \
               ' limit {0}; offset {1};'.format(LIMIT, last)
-    print('last was {0}'.format(last))
+    print('returning {0}'.format(last))
     time.sleep(1)
     return wrap.api_request('games', options)
 
 
-def make_list(end: int = 10000):
+def make_list(end: int = 10):
     last = 0
 
     with open(
@@ -39,7 +33,7 @@ def make_list(end: int = 10000):
         wr = csv.writer(myfile)
         for last in range(0, end, LIMIT):
             wr.writerows(
-                [item['name']]
+                [item['name'], item['id']]
                 for item in json.loads(
                     get_games(last, wrapper).decode('utf-8').replace("'", '"')))
 
