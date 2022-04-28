3ec046cb42339a7b9bde2567d912611352edc383
Jason <leejason1990@gmail.com>
2022-04-28 18:19:01 -0400

    genre script created

--- API scripts/api_genre_request.py
+++ API scripts/api_genre_request.py
@@ -6,10 +6,10 @@ import requests
 igdb_token = os.environ.get('IGDB_TOKEN')
 igdb_id = os.environ.get('IGDB_ID')
 wrapper = IGDBWrapper(igdb_id, igdb_token)
-url = 'https://api.igdb.com/v4/game_modes'
+url = 'https://api.igdb.com/v4/genres'
 offset = 0
-limit = 10
-result = 10
+limit = 50
+result = 50
 
 
 def get_games(offset=offset):
@@ -26,7 +26,7 @@ def get_games(offset=offset):
         print(response)
         offset += limit
 
-        with open('../Data/data_gamemode.json', 'w', encoding='utf-8') as f:
+        with open('../Data/data_genre.json', 'w', encoding='utf-8') as f:
 
             json.dump(response.json(), f, ensure_ascii=False, indent=4)
             if result == offset:
