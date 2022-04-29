c2a2afa30c1f2d551d3b817b6e339faec0cdf341
Jason <leejason1990@gmail.com>
2022-04-29 19:52:47 -0400

    update script

--- API scripts/api_cover_request.py
+++ API scripts/api_cover_request.py
@@ -9,7 +9,7 @@ wrapper = IGDBWrapper(igdb_id, igdb_token)
 url = 'https://api.igdb.com/v4/covers'
 offset = 0
 limit = 500
-result = 1000
+result = 15000
 
 
 def get_games(offset=offset):
@@ -21,16 +21,28 @@ def get_games(offset=offset):
             'Content-Type': 'application/x-www-form-urlencoded',
         }
 
-        data = f'fields id, url; sort id asc; limit {limit}; offset {offset};'
+        data = f'fields id, url; limit {limit}; offset {offset};'
         response = requests.post(url, headers=headers, data=data)
         print(response)
         offset += limit
 
-        with open('../Data/data_url.json', 'w', encoding='utf-8') as f:
-
+        with open('../Data/data_url.json', 'a', encoding='utf-8') as f:
             json.dump(response.json(), f, ensure_ascii=False, indent=4)
             if result == offset:
                 break
 
 
+def correct_file():
+    f1 = open('../Data/data_url.json', 'r')
+    input_data = f1.read()
+    f1.close()
+
+    input_data = input_data.replace('][', ',')
+
+    f2 = open('../Data/data_url.json', 'w')
+    f2.write(input_data)
+    f2.close()
+
+
 get_games()
+correct_file()
--- API scripts/api_game_request.py
+++ API scripts/api_game_request.py
@@ -9,7 +9,7 @@ wrapper = IGDBWrapper(igdb_id, igdb_token)
 url = 'https://api.igdb.com/v4/games'
 offset = 0
 limit = 500
-result = 1000
+result = 2500
 
 
 def get_games(offset=offset):
@@ -23,16 +23,28 @@ def get_games(offset=offset):
 
         data = 'fields id, aggregated_rating, aggregated_rating_count, cover, category, first_release_date, ' \
                'game_modes, genres, name, platforms, rating, rating_count, summary, total_rating, ' \
-               f'total_rating_count; sort id asc; limit {limit}; offset {offset};'
+               f'total_rating_count; sort id asc; where rating >=85; limit {limit}; offset {offset};'
         response = requests.post(url, headers=headers, data=data)
         print(response)
         offset += limit
 
         with open('../Data/data.json', 'a', encoding='utf-8') as f:
-
             json.dump(response.json(), f, ensure_ascii=False, indent=4)
             if result == offset:
                 break
 
 
+def correct_file():
+    f1 = open('../Data/data.json', 'r')
+    input_data = f1.read()
+    f1.close()
+
+    input_data = input_data.replace('][', ',')
+
+    f2 = open('../Data/data.json', 'w')
+    f2.write(input_data)
+    f2.close()
+
+
 get_games()
+correct_file()
