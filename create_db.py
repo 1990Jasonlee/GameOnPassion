ba1128c6684b31488553261a93ae4eae8ae345be
Jason <leejason1990@gmail.com>
2022-04-30 11:23:07 -0400

    db script

--- create_db.py
+++ create_db.py
@@ -4,17 +4,21 @@ conn = psycopg2.connect(
    database="postgres", user='postgres', password='password', host='127.0.0.1', port= '5432'
 )
 conn.autocommit = True
-
 cursor = conn.cursor()
-
 cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'gameon'")
 exists = cursor.fetchone()
 print('database exist')
 if not exists:
     cursor.execute('CREATE DATABASE gameon')
-
-
-
-
-#Closing the connection
 conn.close()
+
+conn = psycopg2.connect(
+   database="gameon", user='postgres', password='password', host='127.0.0.1', port= '5432'
+)
+conn.autocommit = True
+
+create_table = '''CREATE TABLE IF NOT EXISTS tablename(
+                            '' int NOT NULL,
+                            '' int NOT NULL,
+                            '' int NOT NULL,
+                            '' int NOT NULL)'''
