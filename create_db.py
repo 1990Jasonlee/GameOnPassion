aa06f06348bfe64ceb4fa34293f136594006a1c3
Jason <leejason1990@gmail.com>
2022-04-30 16:03:57 -0400

    tables creation

--- create_db.py
+++ create_db.py
@@ -17,8 +17,75 @@ conn = psycopg2.connect(
 )
 conn.autocommit = True
 
-create_table = '''CREATE TABLE IF NOT EXISTS tablename(
-                            '' int NOT NULL,
-                            '' int NOT NULL,
-                            '' int NOT NULL,
-                            '' int NOT NULL)'''
+create_table = '''CREATE TABLE IF NOT EXISTS game_gamemode(
+                            id serial,
+                            user_id int NOT NULL,
+                            mode_id int NOT NULL,
+                            mode1 int NOT NULL,
+                            mode2 int NOT NULL,
+                            mode3 int NOT NULL,
+                            mode4 int NOT NULL,
+                            mode5 int NOT NULL,
+                            PRIMARY KEY(user_id)
+                            )'''
+
+create_table1 = '''CREATE TABLE IF NOT EXISTS game_genre(
+                            id serial,
+                            game_id int NOT NULL,
+                            genre1 int NOT NULL,
+                            genre2 int NOT NULL,
+                            genre3 int NOT NULL,
+                            genre4 int NOT NULL,
+                            genre5 int NOT NULL,
+                            genre6 int NOT NULL,
+                            genre7 int NOT NULL,
+                            genre8 int NOT NULL,
+                            genre9 int NOT NULL,
+                            PRIMARY KEY(game_id)
+                            )'''
+
+create_table2 = '''CREATE TABLE IF NOT EXISTS game_platform(
+                            user_id serial,
+                            plat_id int NOT NULL,
+                            plat_1 int NOT NULL,
+                            plat_2 int NOT NULL,
+                            plat_3 int NOT NULL,
+                            plat_4 int NOT NULL,
+                            plat_5 int NOT NULL,
+                            plat_6 int NOT NULL,
+                            plat_7 int NOT NULL,
+                            plat_8 int NOT NULL,
+                            plat_9 int NOT NULL,
+                            plat_10 int NOT NULL,                            
+                            plat_11 int NOT NULL,
+                            plat_12 int NOT NULL,
+                            plat_13 int NOT NULL,
+                            plat_14 int NOT NULL,
+                            plat_15 int NOT NULL,
+                            plat_16 int NOT NULL,
+                            plat_17 int NOT NULL,
+                            plat_18 int NOT NULL,
+                            plat_19 int NOT NULL,
+                            PRIMARY KEY(plat_id)
+                            )'''
+
+create_table3 = '''CREATE TABLE IF NOT EXISTS gamemode_info(
+                            id serial,
+                            mode_id int NOT NULL,
+                            mode_name VARCHAR(100) NOT NULL,
+                            PRIMARY KEY(mode_id)
+                            )'''
+
+create_table4 = '''CREATE TABLE IF NOT EXISTS genre_info(
+                            id serial,
+                            genre_id int NOT NULL,
+                            genre_name VARCHAR(100) NOT NULL,
+                            PRIMARY KEY(genre_id)
+                            )'''
+
+create_table5 = '''CREATE TABLE IF NOT EXISTS platform_info(
+                            id serial,
+                            platform_id int NOT NULL,
+                            platform_name VARCHAR(100) NOT NULL,
+                            PRIMARY KEY(platform_id)
+                            )'''
