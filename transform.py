00a110b130fea0033bf932aabc56ac81996c360e
Jason <leejason1990@gmail.com>
2022-04-30 20:14:59 -0400

    minor fix

--- transform.py
+++ transform.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import json
 
 df = pd.read_json('./Data/data.json')
 df.set_index("id", inplace=True)
@@ -159,3 +158,94 @@ plat_combined = plat1.add(plat2, fill_value=0).add(plat3, fill_value=0).add(plat
     .add(plat17, fill_value=0).add(plat18, fill_value=0).add(plat19, fill_value=0).replace(np.nan, 0).astype(int)
 plat_combined = plat_combined.rename(columns={'Arcade': 'Arcade Platform'})
 
+# df = df.join(genres_combined).join(plat_combined).join(mode_combined)
+# df_mask = df['category'] == 0
+# df1 = df[df_mask]
+# df_mask1 = df1['Score Rating'] >= 70
+# df2 = df1[df_mask1]
+# gameon_df = df2.drop(columns=['category', 'game_modes', 'genres',
+#                               'platforms', 'total_rating', 'total_rating_count'])
+#
+# gameon_df[['Cover','Release Year', 'Score Rating', 'Adventure', 'Arcade', 'Card & Board Game',
+#   'Fighting', "Hack and slash/Beat 'em up", 'Indie', 'Music', 'Pinball', 'Platform', 'Point-and-click',
+#   'Puzzle', 'Quiz/Trivia', 'Racing', 'Real Time Strategy (RTS)', 'Role-playing (RPG)', 'Shooter',
+#   'Simulator', 'Sport', 'Strategy', 'Tactical', 'Turn-based strategy (TBS)', 'Visual Novel',
+#   '3DO Interactive Multiplayer', 'Acorn Archimedes', 'Acorn Electron', 'Amazon Fire TV', 'Amiga',
+#   'Amiga CD32', 'Amstrad CPC', 'Android', 'Apple II', 'Apple IIGS', 'Arcade Platform', 'Atari 2600',
+#   'Atari 8-bit', 'Atari Jaguar', 'Atari Lynx', 'Atari ST/STE', 'BBC Microcomputer System', 'BlackBerry OS',
+#   'ColecoVision', 'Commodore 16', 'Commodore C64/128', 'Commodore VIC-20', 'DUPLICATE Stadia', 'DVD Player',
+#   'Daydream', 'Donner Model 30', 'Dragon 32/64', 'Dreamcast', 'FM Towns', 'FM-7', 'Family Computer',
+#   'Family Computer Disk System', 'Game Boy', 'Game Boy Advance', 'Game Boy Color', 'Gear VR',
+#   'Google Stadia', 'Intellivision', 'Legacy Mobile Device', 'Linux', 'MSX', 'MSX2', 'Mac', 'N-Gage',
+#   'NEC PC-6000 Series', 'Neo Geo AES', 'Neo Geo CD', 'Neo Geo MVS', 'New Nintendo 3DS', 'Nintendo 3DS',
+#   'Nintendo 64', 'Nintendo DS', 'Nintendo DSi', 'Nintendo Entertainment System (NES)', 'Nintendo GameCube',
+#   'Nintendo Switch', 'OOParts', 'Oculus Quest', 'Oculus Quest 2', 'Oculus Rift', 'Oculus VR',
+#   'OnLive Game System', 'Ouya', 'PC (Microsoft Windows)', 'PC DOS', 'PC-8801', 'PC-98', 'PlayStation',
+#   'PlayStation 2', 'PlayStation 3', 'PlayStation 4', 'PlayStation 5', 'PlayStation Portable',
+#   'PlayStation VR', 'PlayStation Vita', 'S', 'Satellaview', 'Sega CD', 'Sega Game Gear',
+#   'Sega Master System', 'Sega Mega Drive/Genesis', 'Sega Saturn', 'Sharp X1', 'Sharp X68000', 'SteamVR',
+#   'Super Famicom', 'Super Nintendo Entertainment System (SNES)', 'Tapwave Zodiac', 'Tatung Einstein',
+#   'TurboGrafx-16/PC Engine', 'Turbografx-16/PC Engine CD', 'Web browser', 'Wii', 'Wii U',
+#   'Windows Mixed Reality', 'Windows Phone', 'WonderSwan', 'WonderSwan Color', 'Xbox', 'Xbox 360',
+#   'Xbox One', 'Xbox Series X', 'ZX Spectrum', 'iOS', 'Battle Royale', 'Co-operative',
+#   'Massively Multiplayer Online (MMO)', 'Multiplayer', 'Single player', 'Split screen']]=\
+#     gameon_df[['Cover','Release Year', 'Score Rating', 'Adventure', 'Arcade', 'Card & Board Game',
+#   'Fighting', "Hack and slash/Beat 'em up", 'Indie', 'Music', 'Pinball', 'Platform', 'Point-and-click',
+#   'Puzzle', 'Quiz/Trivia', 'Racing', 'Real Time Strategy (RTS)', 'Role-playing (RPG)', 'Shooter',
+#   'Simulator', 'Sport', 'Strategy', 'Tactical', 'Turn-based strategy (TBS)', 'Visual Novel',
+#   '3DO Interactive Multiplayer', 'Acorn Archimedes', 'Acorn Electron', 'Amazon Fire TV', 'Amiga',
+#   'Amiga CD32', 'Amstrad CPC', 'Android', 'Apple II', 'Apple IIGS', 'Arcade Platform', 'Atari 2600',
+#   'Atari 8-bit', 'Atari Jaguar', 'Atari Lynx', 'Atari ST/STE', 'BBC Microcomputer System', 'BlackBerry OS',
+#   'ColecoVision', 'Commodore 16', 'Commodore C64/128', 'Commodore VIC-20', 'DUPLICATE Stadia', 'DVD Player',
+#   'Daydream', 'Donner Model 30', 'Dragon 32/64', 'Dreamcast', 'FM Towns', 'FM-7', 'Family Computer',
+#   'Family Computer Disk System', 'Game Boy', 'Game Boy Advance', 'Game Boy Color', 'Gear VR',
+#   'Google Stadia', 'Intellivision', 'Legacy Mobile Device', 'Linux', 'MSX', 'MSX2', 'Mac', 'N-Gage',
+#   'NEC PC-6000 Series', 'Neo Geo AES', 'Neo Geo CD', 'Neo Geo MVS', 'New Nintendo 3DS', 'Nintendo 3DS',
+#   'Nintendo 64', 'Nintendo DS', 'Nintendo DSi', 'Nintendo Entertainment System (NES)', 'Nintendo GameCube',
+#   'Nintendo Switch', 'OOParts', 'Oculus Quest', 'Oculus Quest 2', 'Oculus Rift', 'Oculus VR',
+#   'OnLive Game System', 'Ouya', 'PC (Microsoft Windows)', 'PC DOS', 'PC-8801', 'PC-98', 'PlayStation',
+#   'PlayStation 2', 'PlayStation 3', 'PlayStation 4', 'PlayStation 5', 'PlayStation Portable',
+#   'PlayStation VR', 'PlayStation Vita', 'S', 'Satellaview', 'Sega CD', 'Sega Game Gear',
+#   'Sega Master System', 'Sega Mega Drive/Genesis', 'Sega Saturn', 'Sharp X1', 'Sharp X68000', 'SteamVR',
+#   'Super Famicom', 'Super Nintendo Entertainment System (SNES)', 'Tapwave Zodiac', 'Tatung Einstein',
+#   'TurboGrafx-16/PC Engine', 'Turbografx-16/PC Engine CD', 'Web browser', 'Wii', 'Wii U',
+#   'Windows Mixed Reality', 'Windows Phone', 'WonderSwan', 'WonderSwan Color', 'Xbox', 'Xbox 360',
+#   'Xbox One', 'Xbox Series X', 'ZX Spectrum', 'iOS', 'Battle Royale', 'Co-operative',
+#   'Massively Multiplayer Online (MMO)', 'Multiplayer', 'Single player',
+#   'Split screen']].apply(lambda x : x.astype(np.int64))
+#
+# header = [['Game Info', 'Game Info', 'Game Info', 'Game Info', 'Game Info', 'Genre', 'Genre', 'Genre', 'Genre',
+# 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre',
+# 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Genre', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform', 'Platform',
+# 'Game Mode', 'Game Mode', 'Game Mode', 'Game Mode', 'Game Mode', 'Game Mode'], [ 'Cover', 'Name', 'Summary',
+# 'Release Year', 'Score Rating', 'Adventure', 'Arcade', 'Card & Board Game', 'Fighting', "Hack and slash/Beat 'em
+# up", 'Indie', 'Music', 'Pinball', 'Platform', 'Point-and-click', 'Puzzle', 'Quiz/Trivia', 'Racing', 'Real Time
+# Strategy (RTS)', 'Role-playing (RPG)', 'Shooter', 'Simulator', 'Sport', 'Strategy', 'Tactical', 'Turn-based
+# strategy (TBS)', 'Visual Novel', '3DO Interactive Multiplayer', 'Acorn Archimedes', 'Acorn Electron', 'Amazon Fire
+# TV', 'Amiga', 'Amiga CD32', 'Amstrad CPC', 'Android', 'Apple II', 'Apple IIGS', 'Arcade Platform', 'Atari 2600',
+# 'Atari 8-bit', 'Atari Jaguar', 'Atari Lynx', 'Atari ST/STE', 'BBC Microcomputer System', 'BlackBerry OS',
+# 'ColecoVision', 'Commodore 16', 'Commodore C64/128', 'Commodore VIC-20', 'DUPLICATE Stadia', 'DVD Player',
+# 'Daydream', 'Donner Model 30', 'Dragon 32/64', 'Dreamcast', 'FM Towns', 'FM-7', 'Family Computer', 'Family Computer
+# Disk System', 'Game Boy', 'Game Boy Advance', 'Game Boy Color', 'Gear VR', 'Google Stadia', 'Intellivision',
+# 'Legacy Mobile Device', 'Linux', 'MSX', 'MSX2', 'Mac', 'N-Gage', 'NEC PC-6000 Series', 'Neo Geo AES', 'Neo Geo CD',
+# 'Neo Geo MVS', 'New Nintendo 3DS', 'Nintendo 3DS', 'Nintendo 64', 'Nintendo DS', 'Nintendo DSi', 'Nintendo
+# Entertainment System (NES)', 'Nintendo GameCube', 'Nintendo Switch', 'OOParts', 'Oculus Quest', 'Oculus Quest 2',
+# 'Oculus Rift', 'Oculus VR', 'OnLive Game System', 'Ouya', 'PC (Microsoft Windows)', 'PC DOS', 'PC-8801', 'PC-98',
+# 'PlayStation', 'PlayStation 2', 'PlayStation 3', 'PlayStation 4', 'PlayStation 5', 'PlayStation Portable',
+# 'PlayStation VR', 'PlayStation Vita', 'S', 'Satellaview', 'Sega CD', 'Sega Game Gear', 'Sega Master System',
+# 'Sega Mega Drive/Genesis', 'Sega Saturn', 'Sharp X1', 'Sharp X68000', 'SteamVR', 'Super Famicom', 'Super Nintendo
+# Entertainment System (SNES)', 'Tapwave Zodiac', 'Tatung Einstein', 'TurboGrafx-16/PC Engine', 'Turbografx-16/PC
+# Engine CD', 'Web browser', 'Wii', 'Wii U', 'Windows Mixed Reality', 'Windows Phone', 'WonderSwan', 'WonderSwan
+# Color', 'Xbox', 'Xbox 360', 'Xbox One', 'Xbox Series X', 'ZX Spectrum', 'iOS', 'Battle Royale', 'Co-operative',
+# 'Massively Multiplayer Online (MMO)', 'Multiplayer', 'Single player', 'Split screen']] gameon_df.columns = header
