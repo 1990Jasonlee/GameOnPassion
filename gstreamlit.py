f8e09ad3d6dda41b5b763eb25f4854c0ceaa2207
Jason <leejason1990@gmail.com>
2022-05-08 00:36:38 -0400

    streamlit working

--- gstreamlit.py
+++ gstreamlit.py
@@ -1,31 +1,22 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 
 df = pd.read_csv('/Users/jason/dev/GameOnPassion/Visual data/game_load_data.csv')
 df.set_index("id", inplace=True)
-
-def recommender(mode, genre, platform):
-    # df_mask1 = ((df['Release Year'] >= year) & (df['Score Rating'] >= rating) & (df[mode]>0) & (df[genre]>0)& (df[platform]>0))
-    df_mask1 = ((df[mode]>0) & (df[genre]>0)& (df[platform]>0))
-    recommend_df = df[df_mask1]
-    recommend_df = recommend_df[['name', 'Release Year', 'Score Rating', 'summary']]
-    recommend_df = recommend_df.sort_values(by=['Score Rating'], ascendng=False)
-    return recommend_df
-
+df = df.rename(columns={'name': 'Name', 'summary': 'Summary'})
 st.title('Video Game Recommender')
 st.sidebar.header('User Input Features')
 
 years_sorted = sorted(list(df['Release Year'].unique()))
-#year = st.sidebar.slider(int(years_sorted[0]), int(years_sorted[-1]),step=int(1))
-#rating = st.sidebar.slider(minimum = 75, maximum = 100)
-mode = st.sidebar.multiselect('Game Mode', ['Single player', 'Battle Royale', 'Co-operative', 'Massively Multiplayer Online (MMO)', 'Multiplayer', 'Split screen'])
-genre = st.sidebar.multiselect('Genre', ['Adventure', 'Arcade', 'Card & Board Game',
+year = st.sidebar.slider('Year', int(years_sorted[0]), int(years_sorted[-1]), step=int(1))
+rating = st.sidebar.slider('Rating', 75, 100)
+mode = st.sidebar.radio('Game Mode', ['Single player', 'Battle Royale', 'Co-operative', 'Massively Multiplayer Online (MMO)', 'Multiplayer', 'Split screen'])
+genre = st.sidebar.selectbox('Genre', ['Adventure', 'Arcade', 'Card & Board Game',
   'Fighting', "Hack and slash/Beat 'em up", 'Indie', 'Music', 'Pinball', 'Platform', 'Point-and-click',
   'Puzzle', 'Quiz/Trivia', 'Racing', 'Real Time Strategy (RTS)', 'Role-playing (RPG)', 'Shooter',
   'Simulator', 'Sport', 'Strategy', 'Tactical', 'Turn-based strategy (TBS)', 'Visual Novel'])
-platform = st.sidebar.selectbox('Platform',['3DO Interactive Multiplayer', 'Acorn Archimedes', 'Acorn Electron', 'Amazon Fire TV', 'Amiga',
+platform = st.sidebar.selectbox('Platform', ['3DO Interactive Multiplayer', 'Acorn Archimedes', 'Acorn Electron', 'Amazon Fire TV', 'Amiga',
   'Amiga CD32', 'Amstrad CPC', 'Android', 'Apple II', 'Apple IIGS', 'Arcade Platform', 'Atari 2600',
   'Atari 8-bit', 'Atari Jaguar', 'Atari Lynx', 'Atari ST/STE', 'BBC Microcomputer System', 'BlackBerry OS',
   'ColecoVision', 'Commodore 16', 'Commodore C64/128', 'Commodore VIC-20', 'DUPLICATE Stadia', 'DVD Player',
@@ -43,3 +34,17 @@ platform = st.sidebar.selectbox('Platform',['3DO Interactive Multiplayer', 'Acor
   'TurboGrafx-16/PC Engine', 'Turbografx-16/PC Engine CD', 'Web browser', 'Wii', 'Wii U',
   'Windows Mixed Reality', 'Windows Phone', 'WonderSwan', 'WonderSwan Color', 'Xbox', 'Xbox 360',
   'Xbox One', 'Xbox Series X', 'ZX Spectrum', 'iOS'])
+
+
+def recommender(year, rating, mode, genre, platform):
+    df_mask1 = ((df['Release Year'] >= year) & (df['Score Rating'] >= rating) & (df[mode] > 0) & (df[genre] > 0) & (df[platform] > 0))
+    recommend_df = df[df_mask1]
+    recommend_df = recommend_df[['Name', 'Release Year', 'Score Rating', 'Summary']]
+    st.dataframe(recommend_df.style.format({'Score Rating': '{:.2f}'}))
+    return recommend_df
+
+
+if st.sidebar.button('Generate recommendations'):
+    st.success('Here are your recommendations')
+    recommender(year, rating, mode, genre, platform)
+
